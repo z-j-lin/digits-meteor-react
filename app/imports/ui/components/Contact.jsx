b10c1d7bc1700e48e402d7cec989a49415ada5bb
zjlin <zijunlin32@gmail.com>
2020-03-31 10:16:46 -1000

    fixed errors

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Contact extends React.Component {
@@ -12,12 +12,12 @@ class Contact extends React.Component {
             <Image
                 floated='right'
                 size='mini'
-                src={this.props.contacts.image}
+                src={this.props.contact.image}
             />
-            <Card.Header>{this.props.contacts.firstName} {this.props.contacts.lastName}</Card.Header>
-            <Card.Meta>{this.props.contacts.address}</Card.Meta>
+            <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
+            <Card.Meta>{this.props.contact.address}</Card.Meta>
             <Card.Description>
-              {this.props.contacts.description}
+              {this.props.contact.description}
 
             </Card.Description>
           </Card.Content>
