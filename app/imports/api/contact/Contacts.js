26b26145b834af94c9952a59f413324dac50402b
zjlin <zijunlin32@gmail.com>
2020-04-01 21:31:38 -1000

    this is not working

---
+++ app/imports/api/contact/Contacts.js
@@ -0,0 +1,23 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Define a Mongo collection to hold the data. */
+const Contacts = new Mongo.Collection('Contacts');
+
+/** Define a schema to specify the structure of each document in the collection. */
+const ContactsSchema = new SimpleSchema({
+  name: String,
+  firstName: String,
+  lastName: String,
+  address: String,
+  image: String,
+  description: String,
+  owner: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Contacts.attachSchema(ContactsSchema);
+
+/** Make the collection and schema available to other code. */
+export { Contacts, ContactsSchema };
--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Contacts } from '../../api/contact/Contacts';
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
@@ -14,5 +14,17 @@ if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+/** Initialize the database with a default contact document. */
+function addContact(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Contacts.insert(data);
+}
+/** Initialize the collection if empty. */
+if (Contacts.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating contact data.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
   }
 }
