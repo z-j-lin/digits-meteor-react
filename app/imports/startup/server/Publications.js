facbc6c28e8f60f89c5867487513a06b957efa24
zjlin <zijunlin32@gmail.com>
2020-04-01 23:23:13 -1000

    push

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -20,7 +20,7 @@ Meteor.publish('StuffAdmin', function publish() {
   return this.ready();
 });
 
-Meteor.publish('Contact', function publish() {
+Meteor.publish('Contacts', function publish() {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Contacts.find({ owner: username });
@@ -29,7 +29,7 @@ Meteor.publish('Contact', function publish() {
 });
 
 /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-Meteor.publish('ContactAdmin', function publish() {
+Meteor.publish('ContactsAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Contacts.find();
   }
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -8,7 +8,7 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
 import ListContacts from '../pages/ListContacts';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
+import ListContactsAdmin from '../pages/ListContactsAdmin';
 import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
@@ -30,7 +30,7 @@ class App extends React.Component {
               <ProtectedRoute path="/list" component={ListContacts}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-              <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
+              <AdminProtectedRoute path="/admin" component={ListContactsAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
